2d5f4ee735936a5fac516798d64e7b866c19b415
nandafatkhur <nandafatkhur@gmail.com>
2021-10-19 21:08:39 +0700

    Update Bangun3D.java

--- Kelompok 1 (Tugas GUI)/src/Bangun3D.java
+++ Kelompok 1 (Tugas GUI)/src/Bangun3D.java
@@ -27,4 +27,49 @@ public class Bangun3D {
         float hasil = 4 * phi * (jari * jari);
         return hasil;
     }
+
+    public float volumekerucut(float jari, float tinggi){
+        float phi = (float) 3.14;
+        float hasil = (float) 1/3 * phi * jari * jari * tinggi;
+        return hasil;
+    }
+    public float luasPerkerucut(float jari, float sisiMiring){
+        float phi = (float) 3.14;
+        float hasil = phi * jari * (jari + sisiMiring);
+        return  hasil;
+    }
+
+    public float volumeLimassegitiga(float alas, float tinggiAlas, float tinggi){
+        float luasAlas = (float) 1/2 * alas * tinggiAlas;
+        float hasil = (float) 1/3 * luasAlas * tinggi;
+        return hasil;
+    }
+    public float luasPerlimassegitiga(float alas, float tinggiAlas, float segitiga1, float segitiga2, float segitiga3){
+        float luasAlas = (float) 1/2 * alas * tinggiAlas;
+        float hasil = luasAlas + segitiga1 + segitiga2 +segitiga3;
+        return  hasil;
+    }
+
+    public float volumeLimassegiempat(float alasSisi, float tinggi){
+        float luasAlas = alasSisi * alasSisi;
+        float hasil = (float) 1/3 * luasAlas * tinggi;
+        return  hasil;
+    }
+    public float luasPerlimassegiempat(float alasSisi, float segitiga1, float segitiga2, float segitiga3){
+        float luasAlas = alasSisi * alasSisi;
+        float hasil = luasAlas * segitiga1 * segitiga2 * segitiga3;
+        return hasil;
+    }
+
+    public float volumePrismasegitiga(float alas, float alasTinggi, float tinggi){
+        float luasAlas = (float) 1/2 * alas * alasTinggi;
+        float hasil = luasAlas * tinggi;
+        return  hasil;
+    }
+    public float luasPerPrismasegitiga(float alas, float alasTinggi, float alasSisimiring, float tinggi){
+        float luasAlas = (float) 1/2 * alas * alasTinggi;
+        float kelilingAlas = alas + alasSisimiring + alasTinggi;
+        float hasil = (2 * luasAlas) + (kelilingAlas * tinggi);
+        return  hasil;
+    }
 }
